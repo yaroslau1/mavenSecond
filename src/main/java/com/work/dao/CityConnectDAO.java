260880e98778475c3e6e2b2c3e729ba9135558c6
samoilovich_y <sam.yar@mail.ru>
2018-12-18 17:00:03 +0300

    some changes 1.8

--- src/main/java/com/work/dao/CityConnectDAO.java
+++ src/main/java/com/work/dao/CityConnectDAO.java
@@ -19,7 +19,7 @@ public class CityConnectDAO implements CityDAO, AutoCloseable {
 
 	private Connection connection;
 	private PreparedStatement getAll = null;
-	private PreparedStatement addValues = null;
+	private PreparedStatement addValues;
 	private PreparedStatement deleteByID = null;
 	private PreparedStatement update = null;
 
@@ -37,7 +37,7 @@ public class CityConnectDAO implements CityDAO, AutoCloseable {
 			Class.forName(driverName).newInstance();
 			connection = DriverManager.getConnection(url, user, pass);
 			getAll = connection.prepareStatement("SELECT ID, Name, CountryCode, Population FROM city");
-			addValues = connection.prepareStatement("INSERT INTO world.city (Name, CountryCode, Population) VALUES (?, ?, ?)");
+			addValues = connection.prepareStatement("INSERT INTO city (Name, CountryCode, Population) VALUES (?, ?, ?)");
 			deleteByID = connection.prepareStatement("DELETE FROM city WHERE id = ?");
 			update = connection.prepareStatement("UPDATE city SET Name = ?, Population = ? WHERE Id = ?");
 		} catch (IOException e) {
@@ -52,6 +52,36 @@ public class CityConnectDAO implements CityDAO, AutoCloseable {
 			throw new DAOException("Error in constructor with SQLQuery", e);
 		}
 	}
+
+    public CityConnectDAO(String path) throws DAOException {
+        Properties property = new Properties();
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        InputStream reader = classLoader.getResourceAsStream(path);
+        try {
+            property.load(reader);
+            String driverName = property.getProperty("driver");
+            String url = property.getProperty("url");
+            String user = property.getProperty("user");
+            String pass = property.getProperty("pass");
+            reader.close();
+            Class.forName(driverName).newInstance();
+            connection = DriverManager.getConnection(url, user, pass);
+            getAll = connection.prepareStatement("SELECT ID, Name, CountryCode, Population FROM city");
+            addValues = connection.prepareStatement("INSERT INTO city (Name, CountryCode, Population) VALUES (?, ?, ?)");
+            deleteByID = connection.prepareStatement("DELETE FROM city WHERE id = ?");
+            update = connection.prepareStatement("UPDATE city SET Name = ?, Population = ? WHERE Id = ?");
+        } catch (IOException e) {
+            throw new DAOException("Error in constructor with file opening", e);
+        } catch (InstantiationException e) {
+            throw new DAOException("Error in constructor: object Class not found", e);
+        } catch (IllegalAccessException e) {
+            throw new DAOException("Error in constructor with access to DB", e);
+        } catch (ClassNotFoundException e) {
+            throw new DAOException("Error in constructor with ClassPath", e);
+        } catch (SQLException e) {
+            throw new DAOException("Error in constructor with SQLQuery", e);
+        }
+    }
 
 	public void close() throws DAOException {
 		SQLException exception = new SQLException("Some errors with closing");
